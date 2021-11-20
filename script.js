323950b172598873abfda3c69237a4c8998b860c
Justin Bliss <bliss1925@gmail.com>
2021-11-20 13:39:06 -0500

    Finished 5 round game

--- script.js
+++ script.js
@@ -18,51 +18,96 @@ function computerPlay() {      //generates a computer option
     return x;    
 }
 
-let playerSelection = prompt("Rock, Paper or Scissors?");
 
-playerSelection = playerSelection.toLowerCase();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let i = 0;
+    for (i; i < 5; i++) {
+        let x = gameRound();
+        if (x == "CPU"){
+            computerScore++;
+        }
+        else if (x == "Player") {
+            playerScore++;
+        }
+    }
 
-while (playerSelection != "rock" && playerSelection !="paper" && playerSelection !="scissors") {  //input check
-    playerSelection = prompt("That's not the game! Please input Rock, Paper or Scissors");
+    if (playerScore > computerScore) {
+        alert("Congratulations! You win!");
+        return 0;
+    }
+    else if (computerScore > playerScore) {
+        alert("Better luck next time :(");
+        return 0;
+    }
+    else {
+        alert("Who likes a tie? Try again!");
+        return 0;
+    }
+
+function gameRound() {
+    let x = prompt("Rock, Paper or Scissors?");
+    let y = computerPlay();
+    x = x.toLowerCase();
+    let roundWin = "";
+    while ( x != "rock" && x !="paper" && x !="scissors") {  //input check
+       x = prompt("That's not the game! Please input Rock, Paper or Scissors");
 }
-debugger;
-const computerSelection = computerPlay();
-game(playerSelection, computerSelection);
-
-function game(x, y) {
     switch (x) {
         case "rock":
             if (y == "paper") {
                 console.log("You lose! Paper beats Rock!");
+                roundWin = "CPU";
+                return roundWin;
             }
             else if (y == "scissors") {
                 console.log("You win! Rock beats Scissors!");
+                roundWin = "Player";
+                return roundWin;
             }
             else {
                 console.log("Oh boy, a tie!");
+                roundWin = "Not a winner";
+                return roundWin;
             }
             break;
         case "paper":
             if (y == "scissors") {
                 console.log("You lose! Paper beats Rock!");
+                roundWin = "CPU";
+                return roundWin;
             }
             else if (y == "rock") {
                 console.log("You win! Rock beats Scissors!");
+                roundWin = "Player";
+                return roundWin;
             }
             else {
                 console.log("Oh boy, a tie!");
+                roundWin = "Not a winner";
+                return roundWin;
             }
             break;
         case "scissors":
             if (y == "rock") {
                 console.log("You lose! Paper beats Rock!");
+                roundWin = "CPU";
+                return roundWin;
             }
             else if (y == "paper") {
                 console.log("You win! Rock beats Scissors!");
+                roundWin = "Player";
+                return roundWin;
             }
             else {
                 console.log("Oh boy, a tie!");
+                roundWin = "Not a winner";
+                return roundWin;
             }
-            break;
+            
+            }
+        }
     }
-}
+
+game();
